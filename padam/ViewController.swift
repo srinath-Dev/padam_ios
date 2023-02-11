b59ede4e5d6ca0de590a01833a489a4a4fedac2c
Srinath A <nathsri258@gmail.com>
2023-02-11 12:47:38 +0530

    project init

---
+++ padam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  padam
+//
+//  Created by Srinath Dev on 11/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
